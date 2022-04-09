f34cb827d602119d2b4d27efaa0f8c386b0f3a24
Ivan <isomov@yellow.myna.spbdns.com>
2022-04-10 00:07:43 +0300

    add st server v1.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # style_transfer_app
+
+## Run local server
+
+```sh run_server.sh```
+
+## Request example
+
+```sh request_example```
---
+++ server.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+import tempfile
+import os
+import torch
+from PIL import Image
+from function import style_transfer, coral
+from torchvision.utils import save_image
+from torchvision import transforms
+
+
+def write_file(data, path):
+    with open(path, "wb") as f:
+        f.write(data.read())
+
+
+class Data(BaseModel):
+    style: UploadFile
+    content: UploadFile
+    alpha: str
+    preserve_color: str
+
+
+decoder = torch.load("data/decoder.pt")
+vgg = torch.load("data/vgg.pt")
+decoder.eval()
+vgg.eval()
+trans = transforms.ToTensor()
+
+app = FastAPI()
+
+
+@app.post("/generate")
+def main(style: UploadFile, content: UploadFile):
+    style_format = style.content_type.split("/")[-1]
+    content_format = content.content_type.split("/")[-1]
+    style_name = "style." + style_format
+    content_name = "content." + content_format
+    result_name = "result.png"
+    # alpha = float(data.alpha)
+    alpha = 1.0
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        style_name = os.path.join(tmp_dir, style_name)
+        content_name = os.path.join(tmp_dir, content_name)
+        # result_name = os.path.join(tmp_dir, result_name)
+        write_file(style.file, style_name)
+        write_file(content.file, content_name)
+        content = trans(Image.open(content_name))
+        style = trans(Image.open(style_name))
+
+    # if data.preserve_color:
+    #    style = coral(style, content)
+    style = style.unsqueeze(0)
+    content = content.unsqueeze(0)
+    with torch.no_grad():
+        output = style_transfer(vgg, decoder, content, style, alpha)
+    save_image(output, result_name)
+    return FileResponse(result_name)
